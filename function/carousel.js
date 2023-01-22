d2c3c78a0e9448b16c7864afb468de8b6c63a024
etosolo322 <etosolo322@yandex.ru>
2023-01-22 22:02:24 +0300

    add carousel

---
+++ function/carousel.js
@@ -0,0 +1,83 @@
+const text1_options = [
+    "Burj Al Arab",
+    "Burj Khalifa",
+    "Innschbruck",
+    "Red desert",
+    "Temple Expiatori de la Sagrada Família",
+    "Venezia"
+  ];
+  const text2_options = [
+    "Dubai",
+    "Dubai",
+    "Austria",
+    "UAE",
+    "Barcelona",
+    "Italy"
+  ];
+  const color_options = ["#EBB9D2", "#FE9968", "#7FE0EB", "#6CE5B1", "#FE9968", "#7FE0EB",];
+  const image_options = [
+    "../assets/images/jpg/alArab.jpg",
+    "../assets/images/jpg/burjHalifa.jpg",
+    "../assets/images/jpg/insbruk.jpg",
+    "../assets/images/jpg/redDesert.jpg",
+    "../assets/images/jpg/segrada.jpg",
+    "../assets/images/jpg/venezia.jpg",
+  ];
+  var i = 0;
+  const currentOptionText1 = document.getElementById("current-option-text1");
+  const currentOptionText2 = document.getElementById("current-option-text2");
+  const currentOptionImage = document.getElementById("image");
+  const carousel = document.getElementById("carousel-wrapper");
+  const mainMenu = document.getElementById("menu");
+  const optionPrevious = document.getElementById("previous-option");
+  const optionNext = document.getElementById("next-option");
+  
+  currentOptionText1.innerText = text1_options[i];
+  currentOptionText2.innerText = text2_options[i];
+  currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+  mainMenu.style.background = color_options[i];
+  
+  optionNext.onclick = function () {
+    i = i + 1;
+    i = i % text1_options.length;
+    currentOptionText1.dataset.nextText = text1_options[i];
+  
+    currentOptionText2.dataset.nextText = text2_options[i];
+  
+    mainMenu.style.background = color_options[i];
+    carousel.classList.add("anim-next");
+    
+    setTimeout(() => {
+      currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+    }, 455);
+    
+    setTimeout(() => {
+      currentOptionText1.innerText = text1_options[i];
+      currentOptionText2.innerText = text2_options[i];
+      carousel.classList.remove("anim-next");
+    }, 650);
+  };
+  
+  optionPrevious.onclick = function () {
+    if (i === 0) {
+      i = text1_options.length;
+    }
+    i = i - 1;
+    currentOptionText1.dataset.previousText = text1_options[i];
+  
+    currentOptionText2.dataset.previousText = text2_options[i];
+  
+    mainMenu.style.background = color_options[i];
+    carousel.classList.add("anim-previous");
+  
+    setTimeout(() => {
+      currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+    }, 455);
+    
+    setTimeout(() => {
+      currentOptionText1.innerText = text1_options[i];
+      currentOptionText2.innerText = text2_options[i];
+      carousel.classList.remove("anim-previous");
+    }, 650);
+  };
+  
